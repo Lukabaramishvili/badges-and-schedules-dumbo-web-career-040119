bd3c0bfb7054460e8dc5ac564f93335c417e46ba
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-18 22:02:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(speakers)
   room = 0 
-  speakers.collect do |name|
+  speakers.each_with_index do |name, index|
     room += 1 
     puts "Hello, #{name}! You'll be assigned to room #{room}!"
   end
