c4387f0f211db850c92854e7f829fe1d354a4dca
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-18 22:23:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,25 +4,25 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(names)
   new_badges = []
-  array.each do |name|
+  names.each do |name|
     new_badges << "Hello, my name is #{name}."
   end
   return new_badges
 end 
 
-def assign_rooms(speakers)
+def assign_rooms(names)
   new_array = []
-  speakers.each_with_index do |name, room|
+  names.each_with_index do |name, room|
     new_array << "Hello, #{name}! You'll be assigned to room #{room + 1}!"
   end
   return new_array
 end 
 
-def printer(outputs)
-  arr_badges = batch_badge_creator(outputs)
-  arr_rooms = assign_rooms(outputs)
+def printer(names)
+  arr_badges = batch_badge_creator(names)
+  arr_rooms = assign_rooms(names)
   arr_badges.each do |string|
     puts string
   end
